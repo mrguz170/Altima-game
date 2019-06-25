cc7a53641bad46a59762a3b213620e67ac2ba94d
gustavo <banuelos.201021581@hotmail.com>
2019-06-25 12:24:02 -0600

    first commit
    se agrego juego

---
+++ Main/Assets/Resources/Scrips/playerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public CharacterController controllerPlayer;
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+        controllerPlayer = GetComponent<CharacterController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 mov = new Vector3(SimpleInput.GetAxis("Horizontal") * speed, 0, SimpleInput.GetAxis("Vertical") * speed);
+
+        controllerPlayer.Move(mov * Time.deltaTime);
+
+	}
+}

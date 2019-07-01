d7db420045d141fc852d5d34a5af100f1bd67146
gustavo <banuelos.201021581@hotmail.com>
2019-07-01 16:22:01 -0600

    update

---
+++ Main/Assets/Assets personalizados/Scripts/enableAltima.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enableAltima : MonoBehaviour {
+
+    public GameObject altima;
+
+    void OnEnable()
+    {
+        //GameObject thePlayer = GameObject.Find("altima");
+        altima.SetActive(false);
+        //thePlayer.GetComponent<RCC_CarControllerV3>().OnEnable();
+        //thePlayer.GetComponent<RCC_CarControllerV3>().KillOrStartEngine();
+        //thePlayer.SetActive(false);
+
+    }
+
+    void OnDisable()
+    {
+        //GameObject thePlayer = GameObject.Find("altima");
+        altima.SetActive(true);
+
+    }
+}
---
+++ Main/Assets/Assets personalizados/Scripts/playInter01.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Playables;
+
+using UnityEngine;
+
+public class playInter01 : MonoBehaviour {
+
+    public PlayableDirector timeline;
+    //public GameObject altima;
+    bool state = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Player" && state == false)
+        {
+            //altima.SetActive(false);
+            timeline.Play();
+            state = true;
+
+        }
+    }
+}

37ad3e843111cdc7f021162adf495f6ecb071e43
mrguz170 <banuelos.201021581@gmail.com>
2019-07-07 19:39:59 -0600

    se terminaron las primeras versiones de las animaciones de las tecnologias

---
+++ Main/Assets/Assets personalizados/Scripts/playMonitor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class playMonitor : MonoBehaviour {
+
+    public PlayableDirector timeline;
+
+    // Use this for initialization
+    void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            timeline.Play();
+        }
+    }
+}
